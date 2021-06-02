4f397bfb6321225db82790bd63942c49a570323c
Tamir Ashkenazy <tamir_ashkenazy@apple.com>
2021-06-02 15:52:23 +0300

    adding files for github pages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import { MenuAppBar } from './components/navbar/navbar'
+
+function App() {
+  return (
+    <React.Fragment>
+      <MenuAppBar />
+      <h1>Blah Blah</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
